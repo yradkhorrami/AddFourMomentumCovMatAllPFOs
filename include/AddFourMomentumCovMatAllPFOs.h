adaacbf9b2683304620ff5715fd96b8e562a69de
Yasser Radkhorrami <yasser.radkhorrami@desy.de>
2020-07-31 00:34:07 +0200

    Add .gitignore

--- include/AddFourMomentumCovMatAllPFOs.h
+++ include/AddFourMomentumCovMatAllPFOs.h
@@ -7,5 +7,26 @@
 #include <EVENT/ReconstructedParticle.h>
 #include "TMatrixD.h"
 
+using namespace lcio ;
+using namespace marlin ;
+
+class AddFourMomentumCovMatAllPFOs : public Processor
+{
+	public:
+
+		virtual Processor*  newProcessor()
+		{
+			return new AddFourMomentumCovMatAllPFOs;
+		}
+		AddFourMomentumCovMatAllPFOs();
+		virtual ~AddFourMomentumCovMatAllPFOs() = default;
+		PFOCorrection(const AddFourMomentumCovMatAllPFOs&) = delete;
+		AddFourMomentumCovMatAllPFOs& operator=(const AddFourMomentumCovMatAllPFOs&) = delete;
+		virtual void init();
+		virtual void processRunHeader();
+		virtual void processEvent( EVENT::LCEvent *pLCEvent );
+		virtual void check( EVENT::LCEvent *pLCEvent );
+		virtual void end();
+
 
 #endif
